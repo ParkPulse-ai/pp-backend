3b3a1b16accb917fb5dcca8473c0c068e918ec3b
vaibhav-vemula <vaibhav25vemula23@gmail.com>
2025-08-17 08:54:10 -0400

    PP V2 app

--- contracts/flowVoting.sol
+++ contracts/flowVoting.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract HederaVoting {
+contract FlowVoting {
     struct Proposal {
         uint256 proposalId;
         string parkName;
